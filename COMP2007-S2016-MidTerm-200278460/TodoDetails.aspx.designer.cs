d75b15cf00257a662b6d3d1ab48e78eae624e4b4
kingnot <kingnot0524@gmail.com>
2016-06-23 13:58:20 -0400

    updated TodoList Page for Read and Delete from DB

--- COMP2007-S2016-MidTerm-200278460/TodoDetails.aspx.designer.cs
+++ COMP2007-S2016-MidTerm-200278460/TodoDetails.aspx.designer.cs
@@ -7,11 +7,63 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace COMP2007_S2016_MidTerm_200278460
-{
-
-
-    public partial class TodoDetails
-    {
+namespace COMP2007_S2016_MidTerm_200278460 {
+    
+    
+    public partial class TodoDetails {
+        
+        /// <summary>
+        /// LastNameTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox LastNameTextBox;
+        
+        /// <summary>
+        /// FirstNameTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox FirstNameTextBox;
+        
+        /// <summary>
+        /// EnrollmentDateTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox EnrollmentDateTextBox;
+        
+        /// <summary>
+        /// RangeValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator RangeValidator1;
+        
+        /// <summary>
+        /// CancelButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button CancelButton;
+        
+        /// <summary>
+        /// SaveButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SaveButton;
     }
 }
--- COMP2007-S2016-MidTerm-200278460/TodoList.aspx.cs
+++ COMP2007-S2016-MidTerm-200278460/TodoList.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// using statements that are required to connect to EF DB
+using COMP2007_S2016_MidTerm_200278460.Models;
+using System.Web.ModelBinding;
+using System.Linq.Dynamic;
+
 namespace COMP2007_S2016_MidTerm_200278460
 {
     public partial class TodoList : System.Web.UI.Page
@@ -12,8 +17,144 @@ namespace COMP2007_S2016_MidTerm_200278460
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            // if loading the page for the first time, populate the todo grid
+            if (!IsPostBack)
+            {
+                Session["SortColumn"] = "TodoID"; // default sort column
+                Session["SortDirection"] = "ASC";
+                // Get the todo data
+                this.GetTodos();
+            }
         }
 
+        /**
+         * <summary>
+         * This method gets the todo data from the DB
+         * </summary>
+         * 
+         * @method GetTodos
+         * @returns {void}
+         */
+        protected void GetTodos()
+        {
+            // connect to EF
+            using (TodoConnection db = new TodoConnection())
+            {
+                string SortString = Session["SortColumn"].ToString() + " " + Session["SortDirection"].ToString();
+
+                // query the Todos Table using EF and LINQ
+                var Todos = (from allTodos in db.Todos
+                                select allTodos);
+
+                // bind the result to the GridView
+                TodosGridView.DataSource = Todos.AsQueryable().OrderBy(SortString).ToList();
+                TodosGridView.DataBind();
+            }
+        }
+
+        /**
+         * <summary>
+         * This event handler deletes a todo item from the db using EF
+         * </summary>
+         * 
+         * @method TodosGridView_RowDeleting
+         * @param {object} sender
+         * @param {GridViewDeleteEventArgs} e
+         * @returns {void}
+         */
+        protected void TodosGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            // store which row was clicked
+            int selectedRow = e.RowIndex;
+
+            // get the selected TodoID using the Grid's DataKey collection
+            int TodoID = Convert.ToInt32(TodosGridView.DataKeys[selectedRow].Values["TodoID"]);
+
+            // use EF to find the selected student in the DB and remove it
+            using (TodoConnection db = new TodoConnection())
+            {
+                // create object of the Student class and store the query string inside of it
+                Todo deletedTodo = (from todoRecords in db.Todos
+                                          where todoRecords.TodoID == TodoID
+                                          select todoRecords).FirstOrDefault();
+
+                // remove the selected student from the db
+                db.Todos.Remove(deletedTodo);
+
+                // save my changes back to the database
+                db.SaveChanges();
+
+                // refresh the grid
+                this.GetTodos();
+            }
+        }
+
+        /**
+         * <summary>
+         * This event handler allows pagination to occur for the Todos List page
+         * </summary>
+         * 
+         * @method TodosGridView_PageIndexChanging
+         * @param {object} sender
+         * @param {GridViewPageEventArgs} e
+         * @returns {void}
+         */
+        protected void TodosGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            // Set the new page number
+            TodosGridView.PageIndex = e.NewPageIndex;
+
+            // refresh the grid
+            this.GetTodos();
+        }
+
+        protected void TodosGridView_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            // get the column to sorty by
+            Session["SortColumn"] = e.SortExpression;
+
+            // Refresh the Grid
+            this.GetTodos();
+
+            // toggle the direction
+            Session["SortDirection"] = Session["SortDirection"].ToString() == "ASC" ? "DESC" : "ASC";
+        }
+
+        protected void TodosGridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (IsPostBack)
+            {
+                if (e.Row.RowType == DataControlRowType.Header) // if header row has been clicked
+                {
+                    LinkButton linkbutton = new LinkButton();
+
+                    for (int index = 0; index < TodosGridView.Columns.Count - 1; index++)
+                    {
+                        if (TodosGridView.Columns[index].SortExpression == Session["SortColumn"].ToString())
+                        {
+                            if (Session["SortDirection"].ToString() == "ASC")
+                            {
+                                linkbutton.Text = " <i class='fa fa-caret-up fa-lg'></i>";
+                            }
+                            else
+                            {
+                                linkbutton.Text = " <i class='fa fa-caret-down fa-lg'></i>";
+                            }
+
+                            e.Row.Cells[index].Controls.Add(linkbutton);
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void PageSizeDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Set the new Page size
+            TodosGridView.PageSize = Convert.ToInt32(PageSizeDropDownList.SelectedValue);
+
+            // refresh the grid
+            this.GetTodos();
+        }
     }
 }
